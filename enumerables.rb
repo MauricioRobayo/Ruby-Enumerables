e47b7dcf37aacf9168d39a1b3af117d995a0eeba
Mauricio Robayo <rfmajo@gmail.com>
2019-08-15 11:34:59 -0500

    Add my_each_with_index

--- enumerables.rb
+++ enumerables.rb
@@ -8,7 +8,20 @@ module Enumerable
       i += 1
     end
   end
+
+  def my_each_with_index
+    i = 0
+    while i < size
+      yield self[i], i
+      i += 1
+    end
+  end
 end
 
 array = [5, 3, 7, 9, 2]
+
+puts '=== my_each ==='
 array.my_each { |val| puts val }
+
+puts '=== my_each_with_index ==='
+array.my_each_with_index { |val, index| puts "#{index} => #{val}" }
