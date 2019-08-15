fde2a8db9c61a68c3298ab7df3f015cc5a90e3f2
Mauricio Robayo <rfmajo@gmail.com>
2019-08-15 17:11:21 -0500

    Better output for examples

--- enumerables.rb
+++ enumerables.rb
@@ -64,24 +64,31 @@ end
 array = [5, 3, 7, 9, 5]
 
 puts "\n> my_each"
+puts "array => #{array}"
 array.my_each { |val| puts val }
 
 puts "\n> my_each_with_index"
+puts "array => #{array}"
 array.my_each_with_index { |val, index| puts "#{index} => #{val}" }
 
 puts "\n> my_select"
+puts "array => #{array}"
 puts(array.my_select { |val| val > 3 })
 
 puts "\n> my_all?"
+puts "array => #{array}"
 puts(array.my_all? { |val| val > 1 })
 
 puts "\n> my_any?"
+puts "array => #{array}"
 puts(array.my_any? { |val| val > 8 })
 
 puts "\n> my_none?"
+puts "array => #{array}"
 puts(array.my_none?(&:even?))
 
 puts "\n> my_count"
+puts "array => #{array}"
 puts(array.my_count { |val| val == 5 })
 puts array.my_count(&:odd?)
 puts array.my_count(&:even?)
